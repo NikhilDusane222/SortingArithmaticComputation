b977004fd911fb9209d117697d2df5b3b9c947eb
Nikhil <you@example.com>
2020-03-26 18:15:58 +0530

    Added: Compute a+b*c

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -2,3 +2,13 @@
 
 printf "Welcome to sorting arithmatic computation"'\n'
 
+# Take input from user
+read -p "Enter A value : " a
+read -p "Enter B value : " b
+read -p "Enter C value : " c
+
+printf "$a \n"
+printf "$b \n"
+printf "$c \n"
+
+result1=$( echo "scale=2 ; $a+$b*$c" | bc -l )
